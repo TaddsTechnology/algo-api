456f5f2a62756d88cd75f9a5779efb4507858d3a
TaddsTechnology <TaddsTeam@users.noreply.huggingface.co>
2026-03-04 06:10:35 +0000

    update kite_config.py

--- kite_config.py
+++ kite_config.py
@@ -11,4 +11,4 @@ TOTP_SECRET = "SV4FAGXVEHDZ6X4KO4JA3P2JL6SCSDIU"
 # PIN = "your_static_pin_here"
 
 # This will be populated automatically
-ACCESS_TOKEN = "GkLdSrl7I0o7DVx7FuBaItwGYw0He3n0"
+ACCESS_TOKEN = "C2wfDib3nl9pcR93GYDpwhNlPYLY5oVe
